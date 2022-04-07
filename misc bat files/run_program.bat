2275cb3639b62ebc2caae9dd2db1e6d205230914
Scott Song <ssong21@uw.edu>
2022-04-07 03:06:43 +0000

    Removing some folders

--- misc bat files/run_program.bat
+++
@@ -1,6 +0,0 @@
-@echo ,on
-:: %1 = name of dataset, %2 = path to csv, %3 = path to output
-call %UserProfile%\Miniconda3-d\Scripts\activate.bat
-python run_program.py %1 %2 %3 --bm --bmo --im --ptid --imgid
-echo "Program finished running!"
-pause
